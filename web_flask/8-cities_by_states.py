7406697db0a291748e28538ac87a3fc2430d8f56
Dohessiekan <116545641+Dohessiekan@users.noreply.github.com>
2023-10-20 14:35:21 +0200

    Create 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+
+app = Flask(_name_)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_route():
+    """Comment"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0')
